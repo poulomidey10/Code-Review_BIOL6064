5b4d55bae23d9d59f6795f1155d1ca19ec699f05
poulomidey10 <144273808+poulomidey10@users.noreply.github.com>
2023-11-04 01:33:32 -0400

    Update Rcode_review.R

--- Rcode_review.R
+++ Rcode_review.R
@@ -1,4 +1,11 @@
+## Research Question is there any significant difference between root and shoot lignin content
 library(readxl)
-metals<-read_excel("C:/Users/Poulomi Dey/Documents\M.S Thesis 2020/Thesis/Chapter 2/Data and Graphs/metalsdata.xlsx")
-View(metalsdata)
-
+metals<-read_excel("C:/Users/Poulomi Dey/Downloads/AM,PD_CC Lignin to N_BDS(sorted).xlsx")
+View(metals)
+metals <- na.omit(metals)
+X <- metals$`Root lignin:N`
+Y <- metals$`Shoot lignin:N`
+anovafit<-aov(X ~ Y)
+anova(anovafit)
+plot (X,Y, main = "Root v/s Shoot lignin:N ratio",
+      xlab= "Root Lignin:N", ylab = "Shoot lignin:N")
